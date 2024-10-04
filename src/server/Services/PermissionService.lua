9f0e4b7f9b11c209a5456571b603dac597684a04
LarryCreating <159762081+LarryCreating@users.noreply.github.com>
2024-10-04 21:24:10 +1000

    Finish PermissionService.lua

---
+++ src/server/Services/PermissionService.lua
@@ -0,0 +1,144 @@
+--[[
+    @classmod PermissionService.lua
+    @author LarryCreating
+    @date 04/10/2024
+]]
+
+local ReplicatedStorage = game:GetService("ReplicatedStorage")
+local Players = game:GetService("Players")
+
+local import = require(ReplicatedStorage.Packages.import)
+
+local Knit = import("packages/Knit")
+
+local PermissionService = Knit.CreateService({
+	Name = "PermissionService",
+	Client = {},
+})
+
+local VERBOSE_LOGS = true
+
+-- @staticfunction PermissionService:HasPermission
+function PermissionService.Client:HasPermission(Player, Data): () -> boolean
+	if typeof(Data) == "Instance" then
+		if Data:IsA("ObjectValue") then
+			Data = Data.Value
+		end
+		if Data:IsA("ModuleScript") then
+			Data = require(Data)
+		end
+	end
+
+	if typeof(Player) == "number" then
+		for i, PlayerObject in ipairs(Players:GetPlayers()) do
+			if PlayerObject.UserId == Player then
+				Player = PlayerObject
+			end
+		end
+
+		if typeof(Player) == "number" then
+			return false
+		end
+	end
+
+	local RequireAll = not not (Data and Data.RequireAll)
+
+	for _, PermissionSegment in pairs(Data) do
+		if type(PermissionSegment) == "table" then
+			if self:HasPermission(Player, PermissionSegment, true) then
+				if not RequireAll then
+					return true
+				end
+			end
+		elseif type(PermissionSegment) == "string" then
+			local returnOpposite = false
+
+			if string.sub(PermissionSegment, 0, 1) == "!" then
+				returnOpposite = true
+				PermissionSegment = string.sub(PermissionSegment, 2)
+			end
+
+			local Split = string.split(PermissionSegment, ":")
+			local FunctionName = Split[1]
+
+			if PermissionService.Functions[FunctionName] then
+				local hasPerm = PermissionService.Functions[FunctionName](Player, self, table.unpack(Split, 2))
+
+				if returnOpposite then
+					hasPerm = not hasPerm
+				end
+
+				if hasPerm then
+					if not RequireAll then
+						return true
+					end
+				elseif RequireAll then
+					return false
+				end
+			elseif RequireAll then
+				warn(
+					'Permission function with the name "' .. FunctionName .. '" was not found (ReqAll).',
+					debug.traceback()
+				)
+				return false
+			end
+		elseif typeof(PermissionSegment) == "function" then
+			local success, result = pcall(PermissionSegment, Player)
+			local PassedCheck = success and (result == true)
+
+			if PassedCheck and not RequireAll then
+				return true
+			elseif not PassedCheck and RequireAll then
+				return false
+			end
+		end
+	end
+
+	return RequireAll
+end
+
+-- @staticfunction PermissionService:RegisterFunction
+function PermissionService:RegisterFunction(Name, Function): ()
+	self.Functions[Name] = Function
+end
+
+-- @staticfunction PermissionService:RegisterFunctionsIn
+function PermissionService:RegisterFunctionsIn(Folder): ()
+	local Children = Folder:GetChildren()
+	for i, Module in pairs(Children) do
+		if Module:IsA("ModuleScript") then
+			local success, result = pcall(function()
+				return self:RegisterFunction(Module.Name, require(Module))
+			end)
+			if success and VERBOSE_LOGS then
+				print("Registered permission function:", Module.Name, tostring(i) .. "/" .. tostring(#Children))
+			elseif VERBOSE_LOGS then
+				warn('Failed to register permission function "' .. Module.Name .. '":')
+			end
+		end
+	end
+	print("Registered Permissions functions in", Folder)
+end
+
+-- @staticfunction PermissionService:init
+function PermissionService:init()
+	self.Initialising = true
+	self:RegisterFunctionsIn(import("shared/Permissions"))
+	self.HasInitialised = true
+	self.Initialising = false
+end
+
+-- @staticfunction PermissionService:KnitStart
+function PermissionService:KnitStart(): ()
+	if not self.HasInitialised then
+		if self.Initialising then
+			repeat
+				task.wait()
+			until self.HasInitialised
+		else
+			self:init()
+		end
+	end
+end
+
+return PermissionService
