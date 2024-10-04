b5fc5b0bcf6dfa4a41a8957592d88bbaeb50628d
LarryCreating <159762081+LarryCreating@users.noreply.github.com>
2024-10-04 12:36:07 +1000

    Begin TestService.lua

---
+++ src/server/Services/TestService.lua
@@ -0,0 +1,23 @@
+--[[
+    @classmod TestService.lua
+    @author LarryCreating
+    @date 04/10/2024
+]]
+
+local ReplicatedStorage = game:GetService("ReplicatedStorage")
+
+local import = require(ReplicatedStorage.Packages.import)
+
+local Knit = import("packages/Knit")
+
+local TestService = Knit.CreateService({
+	Name = "TestService",
+	Client = {},
+})
+
+-- @staticfunction TestService:KnitStart
+function TestService:KnitStart(): ()
+	print("Knit works!")
+end
+
+return TestServic
