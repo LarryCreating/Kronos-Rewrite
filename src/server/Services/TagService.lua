fe350b90543097ae4439869f26acfd50052c93d4
LarryCreating <159762081+LarryCreating@users.noreply.github.com>
2024-10-07 20:53:11 +1100

    Finish TagService.lua

---
+++ src/server/Services/TagService.lua
@@ -0,0 +1,37 @@
+--[[
+    @classmod TagService.lua
+    @author LarryCreating
+    @date 07/10/2024
+]]
+
+local ReplicatedStorage = game:GetService("ReplicatedStorage")
+local CollectionService = game:GetService("CollectionService")
+
+local import = require(ReplicatedStorage.Packages.import)
+
+local Knit = import("packages/Knit")
+
+local TagService = Knit.CreateService({
+	Name = "TagService",
+	Client = {},
+})
+
+-- @staticfunction TagService:BindToTag
+function TagService:BindToTag(Tag, Callback): ()
+	task.spawn(function()
+		for _, Item in ipairs(CollectionService:GetTagged(Tag)) do
+			task.spawn(function()
+				Callback(Item)
+			end)
+		end
+	end)
+
+	return CollectionService:GetInstanceAddedSignal(Tag):Connect(Callback)
+end
+
+-- @staticfunction TagService:BindToTagRemoved
+function TagService:BindToTagRemoved(Tag, Callback): ()
+	return CollectionService:GetInstanceRemovedSignal(Tag):Connect(Callback)
+end
+
+return TagService
